09e6578365e255160047919ce5983f97d61c900d
ugniususc <ugniususc@gmail.com>
2024-06-07 23:33:51 +0300

    register ir login, pilnai login negalejau padaryt nes mongo uzluzo

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,121 @@
+import axios from "axios";
+import { createContext, useContext, useReducer } from "react";
+
+const AuthContext = createContext();
+
+const BASE_URL = "http://localhost:5000/api/users";
+
+const initialState = {
+  username: "",
+  email: "",
+  password: "",
+  repeatPassword: "",
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "register":
+      return {
+        ...state,
+        username: action.payload,
+        email: action.payload,
+        password: action.payload,
+        repeatPassword: action.payload,
+        isAuthenticated: true,
+      };
+    case "login":
+      return {
+        ...state,
+        email: action.payload,
+        password: action.payload,
+        isAuthenticated: true,
+      };
+    case "logout":
+      return {
+        ...state,
+        username: "",
+        email: "",
+        password: "",
+        repeatPassword: "",
+        isAuthenticated: false,
+      };
+
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+function AuthProvider({ children }) {
+  const [
+    { username, email, password, repeatPassword, isAuthenticated },
+    dispatch,
+  ] = useReducer(reducer, initialState);
+
+  async function register(username, email, password, repeatPassword) {
+    try {
+      const res = await axios.post(BASE_URL, {
+        username,
+        email,
+        password,
+      });
+
+      dispatch({
+        type: "register",
+        payload: {
+          username,
+          email,
+          password,
+          repeatPassword,
+        },
+      });
+
+      console.log(res);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async function login() {
+    try {
+      const res = await axios.post(`${BASE_URL}/login`, {
+        email,
+        password,
+      });
+
+      console.log(res);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        username,
+        email,
+        password,
+        repeatPassword,
+        isAuthenticated,
+        register,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined)
+    throw new Error("AuthContext was used outside the AuthProvider");
+  return context;
+}
+
+export { AuthProvider, useAuth };
