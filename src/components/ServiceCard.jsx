dceaa4f38b394924e64dd335fc6fc842a10ec13f
LukasLapienis <lapienislukasp@gmail.com>
2024-06-10 19:52:34 +0300

    added delete

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,3 +1,4 @@
+import { DeleteButton } from "./DeleteButton";
 import {
   Card,
   CardHeader,
@@ -8,11 +9,13 @@ import {
 } from "./ui/card";
 import Rating from "./ui/rating";
 
-
 const ServiceCard = ({ service }) => {
+  const user = JSON.parse(localStorage.getItem("userData"));
+
   return (
     <Card className="flex flex-col justify-between">
-      <Rating service={service}/>
+      <Rating service={service} />
+      <DeleteButton id={service._id} />
       <CardHeader>
         <div>
           <CardTitle>{service.serviceName}</CardTitle>
