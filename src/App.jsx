77281df06c259dc6673c53f672130357aff13982
ugniususc <ugniususc@gmail.com>
2024-06-10 18:03:15 +0300

    ServicesContext

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,38 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import CreateService from "./pages/CreateService";
 import ManageServices from "./pages/ManageServices";
-import React from "react";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { AuthProvider } from "./contexts/AuthContext";
+import { ServiceProvider } from "./contexts/ServicesContext";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route
-            path="create"
-            element={
-              <ProtectedRoute>
-                <CreateService />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="manage"
-            element={
-              <ProtectedRoute>
-                <ManageServices />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="*" element={<p>There's nothing here: 404!</p>} />
-        </Routes>
+        <ServiceProvider>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route
+              path="create"
+              element={
+                <ProtectedRoute>
+                  <CreateService />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="manage"
+              element={
+                <ProtectedRoute>
+                  <ManageServices />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="*" element={<p>There's nothing here: 404!</p>} />
+          </Routes>
+        </ServiceProvider>
       </AuthProvider>
     </BrowserRouter>
   );
--- src/pages/CreateService.jsx
+++ src/pages/CreateService.jsx
@@ -3,16 +3,41 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useAuth } from "@/contexts/AuthContext";
+import { useService } from "@/contexts/ServicesContext";
 import { useNavigate } from "react-router-dom";
 
 const CreateService = () => {
   const navigate = useNavigate();
-
   const { isAuthenticated } = useAuth();
+
+  const {
+    createAd,
+    name,
+    surname,
+    specialization,
+    imageUrl,
+    serviceName,
+    city,
+    dispatch,
+  } = useService();
 
   if (!isAuthenticated) {
     navigate("/");
     return null;
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await createAd();
+    navigate("/");
+  }
+
+  function handleChange(e) {
+    const { id, value } = e.target;
+    dispatch({
+      type: "ads/create",
+      payload: { [id]: value },
+    });
   }
 
   return (
@@ -22,46 +47,74 @@ const CreateService = () => {
           <CardTitle className="text-2xl">Create New Service</CardTitle>
         </CardHeader>
         <CardContent>
-          <div className="grid gap-4">
+          <form className="grid gap-4" onSubmit={handleSubmit}>
             <div className="grid gap-2">
               <Label htmlFor="name">Name</Label>
-              <Input id="name" type="text" required />
+              <Input
+                id="name"
+                type="text"
+                required
+                value={name}
+                onChange={handleChange}
+              />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="Surname">Surname</Label>
-              <Input id="Surname" type="text" required />
+              <Input
+                id="Surname"
+                type="text"
+                required
+                value={surname}
+                onChange={handleChange}
+              />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="specialization">Specialization</Label>
-              <Input id="specialization" type="text" required />
+              <Input
+                id="specialization"
+                type="text"
+                required
+                value={specialization}
+                onChange={handleChange}
+              />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="imageUrl">Image Url</Label>
-              <Input id="imageUrl" type="text" required />
+              <Input
+                id="imageUrl"
+                type="text"
+                required
+                value={imageUrl}
+                onChange={handleChange}
+              />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="serviceName">Service Name</Label>
-              <Input id="serviceName" type="text" required />
+              <Input
+                id="serviceName"
+                type="text"
+                required
+                value={serviceName}
+                onChange={handleChange}
+              />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="city">City</Label>
-              <Input id="city" type="text" required />
+              <Input
+                id="city"
+                type="text"
+                required
+                value={city}
+                onChange={handleChange}
+              />
             </div>
-            <Button
-              type="submit"
-              className="w-full"
-              onClick={() => navigate("/")}
-            >
+            <Button type="submit" className="w-full">
               Create Service
             </Button>
-            <Button
-              onClick={() => navigate("/")}
-              variant="outline"
-              className="w-full"
-            >
+            <Button variant="outline" className="w-full">
               Cancel
             </Button>
-          </div>
+          </form>
         </CardContent>
       </Card>
     </div>
