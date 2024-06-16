3bf40468773f97b5926d5c988f0004d0ebaff054
LukasLapienis <lapienislukasp@gmail.com>
2024-06-16 21:44:43 +0300

    added services services

---
+++ src/services/servicesServices.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+//GET Services
+export const getServices = (setData) => {
+  axios
+    .get(`http://localhost:5000/api/service`)
+    .then((res) => {
+      setData(res.data);
+    })
+    .catch((err) => console.error(err));
+};
+
+//POST Service
+export const saveService = (data, userToken, setData) => {
+  axios
+    .post(`http://localhost:5000/api/service`, data, {
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+        "Content-Type": "application/json",
+      },
+    })
+    .then((res) => getServices(setData))
+    .catch((err) => console.error(err));
+};
+//Update Service
+export const updateService = (data, serviceId, userToken, setData) => {
+  axios
+    .put(`http://localhost:5000/api/service/${serviceId}`, data, {
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+        "Content-Type": "application/json",
+      },
+    })
+    .then((res) => getServices(setData))
+    .catch((err) => console.error(err));
+};
+// Delete Service
+export const deleteService = (serviceId, userToken, setData) => {
+  axios
+    .delete(`http://localhost:5000/api/ads/${serviceId}`, {
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+        "Content-Type": "application/json",
+      },
+    })
+    .then((res) => getServices(setData))
+    .catch((err) => console.error(err));
+};
